ed6e41d4254bd5647a9b89176e7adfad26e9b875
olivan <oleh.ivantsiv@smile-ukraine.com>
2020-05-28 15:56:53 +0300

    rename assets

--- SmileProductReviewBundle.php
+++ SmileProductReviewBundle.php
@@ -3,18 +3,11 @@
 namespace Smile\Bundle\ProductReviewBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
  * Class SmileProductReviewBundle
  */
 class SmileProductReviewBundle extends Bundle
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-    }
+
 }
